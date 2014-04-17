16b28e1cbb70782dd38b8e97b431d8fb3710733c
Shisei Hanai <ruimo.uno@gmail.com>
2014-04-17 23:54:46 +0900

    Fix test sample

--- module_b/src/test/java/foo/BarTest.java
+++ module_b/src/test/java/foo/BarTest.java
@@ -1,11 +1,11 @@
 package foo;
 
 import org.junit.Test;
-import javax.servlet.http.*;
+import javax.xml.ws.http.HTTPException;
 import foo.Foo;
 
 public class BarTest {
     @Test public void test() throws Exception {
-        Cookie cookie = new Cookie("foo", "bar");
+        HTTPException e = new HTTPException(0);
     }
 }
