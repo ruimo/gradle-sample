36b951f6e40687086f1b023b32b1f13e02e61ed9
Shisei Hanai <ruimo.uno@gmail.com>
2014-04-18 11:33:30 +0900

    Fix unit test to detect javaee library is present or not.

--- module_b/src/test/java/foo/BarTest.java
+++ module_b/src/test/java/foo/BarTest.java
@@ -1,11 +1,11 @@
 package foo;
 
 import org.junit.Test;
-import javax.xml.ws.http.HTTPException;
+import javax.enterprise.inject.InjectionException;
 import foo.Foo;
 
 public class BarTest {
     @Test public void test() throws Exception {
-        HTTPException e = new HTTPException(0);
+        InjectionException e = new InjectionException();
     }
 }
